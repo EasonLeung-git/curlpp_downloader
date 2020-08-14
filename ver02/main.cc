c56955e9fbe19d242b29c7012bac05e54c0a1ccf
Eason Leung <liangyusheng@myself.com>
2020-08-14 13:10:38 +0800

    add ver02

--- ver02/main.cc
+++ ver02/main.cc
@@ -3,19 +3,29 @@
 
 #include <iostream>
 #include <string>
+#include <thread>
 #include "CUrlppDownloader.hh"
 
-static std::string g_url = "ftp://10.1.10.84/test_file.200MB.1";
+static std::string g_url_root = "ftp://10.1.10.84/"; // test_file.200MB.1";
+
+void downloader_func(std::string _url, std::string _file_name)
+{
+    CUrlppDownloader downloader;
+    downloader.setup(_url, _file_name);
+    downloader.installDefaultProgressFunction();
+    downloader.installDefaultWriteFunction();
+    downloader.run();
+}
 
 int main(int argc, char *argv[])
 {
     (void) argc, (void) argv;
 
-    CUrlppDownloader downloader;
-    downloader.setup(::g_url, "test_file.200MB.1");
-    downloader.installDefaultProgressFunction();
-    downloader.installDefaultWriteFunction();
-    downloader.run();
+    std::thread t1(downloader_func, g_url_root + "test_file.200MB.1", "test_file.200MB.1");
+    std::thread t2(downloader_func, g_url_root + "test_file.200MB.2", "test_file.200MB.2");
+
+    t1.join();
+    t2.join();
 
     std::cout << "\ndone.\n";
 
